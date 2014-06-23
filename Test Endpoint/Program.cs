d16f3664edc878c3a2b8c1657778dd7c64ea8db2
Eric Arndt <earndt@preemptive.com>
2014-06-23 13:27:39 -0400

    Updated to version 2.0

--- Test Endpoint/Program.cs
+++ Test Endpoint/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2013 PreEmptive Solutions; All Right Reserved, http://www.preemptive.com/
+﻿// Copyright (c) 2014 PreEmptive Solutions; All Right Reserved, http://www.preemptive.com/
 //
 // This source is subject to the Microsoft Public License (MS-PL).
 // Please see the License.txt file for more information.
@@ -9,46 +9,105 @@
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
 // PARTICULAR PURPOSE.
 
+using System.Threading.Tasks;
+using Mono.Options;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Test_Endpoint
 {
     public class Program
     {
-        private const int minPort = 0;
-        private const int maxPort = 65535;
+        private const int MIN_PORT = 0;
+        private const int MAX_PORT = 65535;
+        private const int DEFAULT_PORT = 8080;
+        private const int DEFAULT_LISTENERS_PER_CPU = 4;
+
         public static void Main(string[] args)
         {
-            if (args.Any(x => x == "/?"))
+            int port = DEFAULT_PORT;
+            int listeners = DEFAULT_LISTENERS_PER_CPU * Environment.ProcessorCount;
+            bool fail = false;
+            bool perf = false;
+
+            var options = new OptionSet()
             {
-                Console.WriteLine("Starts up a test endpoint for debugging. Defaults to localhost:8080.");
-                Console.WriteLine();
-                Console.WriteLine("ENDPOINT [/p:portnum] [/h:hostname] [/f]");
-                Console.WriteLine();
-                Console.WriteLine("/h:hostname \t Specifies the hostname for the endpoint to use.");
-                Console.WriteLine("/p:portnum  \t Specifies the port number for the endpoint to use.");
-                Console.WriteLine("/f          \t Causes the endpoint to always return the 500 network response code.");
-                
-                return;
+                { "h|?", v => DieUsage(0) },
+                { "p=", portString => port = ParsePort(portString) },
+                { "l=", listenersString => listeners = ParseListeners(listenersString) },
+                { "f", flag => fail = (flag != null) },
+                { "perf", flag => perf = (flag != null) },
+            };
+
+            List<string> extraArgs = options.Parse(args);
+            if (extraArgs.Count != 0)
+            {
+                DieUsage(1);
             }
 
-            int port = 8080;
+            //required to make async work from a console app
+            Task.WaitAll(MainAsync(port, listeners, fail, perf));
+        }
 
-            if (args.Any(x => x.StartsWith("/p:")))
+        private static void DieUsage(int exitCode)
+        {
+            var stream = Console.Out;
+            if (exitCode != 0)
             {
-                if (!int.TryParse(args.First(x => x.StartsWith("/p:")).Split(':')[1], out port) || port < minPort || port > maxPort)
-                {
-                    Console.WriteLine("Invalid port specified.");
-                    return;
-                }
+                stream = Console.Error;
             }
             
-            string host = args.Any(x => x.StartsWith("/h:")) ? args.First(x => x.StartsWith("/h:")).Split(':')[1] : "localhost";
-            bool fail = args.Contains("/f");
+            stream.WriteLine("USAGE:");
+            stream.WriteLine("endpoint.exe [/h] [/p:portnum] [/l:listeners] [/f]");
+            stream.WriteLine();
+            stream.WriteLine("/h          \t Prints this message.");
+            stream.WriteLine("/p:portnum  \t Specifies the port number to use (default {0}).", DEFAULT_PORT);
+            stream.WriteLine("/l:listeners\t Specifies the number of connection listeners (default {0} per CPU)", DEFAULT_LISTENERS_PER_CPU);
+            stream.WriteLine("/f          \t Causes the endpoint to always return the 500 network response code.");
 
-            var ss = new SimpleServer(host, port, fail);
-            Console.WriteLine("Listening on {0}", ss.EndPoint);
+            Environment.Exit(exitCode);
+        }
+
+        static int ParsePort(string portString)
+        {
+            int port;
+            if (!int.TryParse(portString, out port) || port < MIN_PORT || port > MAX_PORT)
+            {
+                Console.WriteLine("Invalid port specified.");
+                Environment.Exit(1);
+            }
+
+            return port;
+        }
+
+        static int ParseListeners(string listenersString)
+        {
+            int listeners;
+            if (!int.TryParse(listenersString, out listeners) || listeners < 1)
+            {
+                Console.WriteLine("Invalid number of listeners specified.");
+                Environment.Exit(1);
+            }
+
+            return listeners;
+        }
+
+        private async static Task<bool> MainAsync(int port, int listeners, bool fail, bool perf)
+        {
+            try
+            {
+                //have to await so we can catch port-binding exceptions
+                await new SimpleServer(port, listeners, fail, perf).Start();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                Console.Error.WriteLine();
+                Console.Error.WriteLine("Unable to start server. Is the port in use?");
+                Environment.Exit(1);
+            }
+
+            return true;
         }
     }
 }
