3316b380b78c5de6e6c15e23b4bc62f64caf89f6
Joseph Sewell <jsewell@preemptive.com>
2015-01-02 15:45:44 -0500

    Fix up assembly info, including copyright year

--- Test Endpoint/Properties/AssemblyInfo.cs
+++ Test Endpoint/Properties/AssemblyInfo.cs
@@ -1,18 +1,23 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test Endpoint")]
+[assembly: AssemblyTitle("PreEmptive Analytics Test Endpoint")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test Endpoint")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: AssemblyProduct("PreEmptive Analytics Test Endpoint")]
+[assembly: AssemblyCompany("PreEmptive Solutions, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2014 PreEmptive Solutions, LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,4 +38,3 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
