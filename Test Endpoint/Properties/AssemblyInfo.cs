70e7b50f3bebda821876f3e7e9792a5e16d66f91
Joseph Sewell <jsewell@preemptive.com>
2015-12-17 16:29:18 -0500

    Add source licensing statement to AssemblyInfo.cs

--- Test Endpoint/Properties/AssemblyInfo.cs
+++ Test Endpoint/Properties/AssemblyInfo.cs
@@ -1,4 +1,15 @@
-﻿using System.Reflection;
+﻿// Copyright (c) 2015 PreEmptive Solutions; All Right Reserved, http://www.preemptive.com/
+//
+// This source is subject to the Microsoft Public License (MS-PL).
+// Please see the License.txt file for more information.
+// All other rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+// KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
